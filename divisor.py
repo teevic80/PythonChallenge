f29c00a1b5ea4f7128c9fa8109d6a0d05e127654
teevic80 <ogotvictoria@gmail.com>
2019-07-10 23:26:10 +0300

    Fixed bugs!

--- divisor.py
+++ divisor.py
@@ -4,15 +4,8 @@ x = range(1, 100)
 
 var = input("Type a number: ")
 
-while True:
-    for num in x:
-        if int(var) <= 100 and num % int(var) == 0:
-            print(x)
+for num in x:
+    if int(var) <= 100 and num % int(var) == 0:
+        print(num)
 
 
-    
-    
-    
-    
-    #  < 100 and num % var == 0:
-    #     print(num)
