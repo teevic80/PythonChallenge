06678f4e1ae42ddee1d928cc5676987d76963717
teevic80 <ogotvictoria@gmail.com>
2019-07-10 23:21:35 +0300

    Tried a while loop.

--- divisor.py
+++ divisor.py
@@ -4,6 +4,15 @@ x = range(1, 100)
 
 var = input("Type a number: ")
 
-for num in x:
-    if num in var < 100 and num % var == 0:
-        print(num)
+while True:
+    for num in x:
+        if int(var) <= 100 and num % int(var) == 0:
+            print(x)
+
+
+    
+    
+    
+    
+    #  < 100 and num % var == 0:
+    #     print(num)
