7e0aa30e14cc3f0266b1667d4168ae495aa8a676
teevic80 <ogotvictoria@gmail.com>
2019-07-14 00:27:17 +0300

    This program checks if a given word reads the same in reverse.

--- palindrome.py
+++ palindrome.py
@@ -1,2 +1,11 @@
 #This program returns a string in reverse and checks if it's a palindrome or not.
 #A palindrome is a word that has the same letters both backwards and forwards.
+
+pali = input("Type a word: ")
+
+pali_reverse = pali[::-1]
+
+if pali == pali_reverse:
+    print(pali_reverse, ": Your word is a Palindrome!!!")
+else:
+    print(pali_reverse, ": Your word is not a Palindrome.")
