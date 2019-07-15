f162d957ef4c74480e5f6ca88b69e0148ad16eca
teevic80 <ogotvictoria@gmail.com>
2019-07-15 23:56:11 +0300

    Added Import random and our list values.

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,6 +1,13 @@
 #This a simple rock paper scissors game.
 
+import random
+
+game_values = ("Rock", "Paper", "Scissors")
+
 game_start = (input("Welcome to Rock Paper Scissors! Would you like to play? "))
 
+
 while True:
+    if game_start == "yes":
+        print(input("Rock, Paper, Scissors?"))
     
