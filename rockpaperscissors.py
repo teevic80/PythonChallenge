e0c2f55bf7608cd33809d483638403ad523ee282
teevic80 <ogotvictoria@gmail.com>
2019-07-16 01:18:34 +0300

    Tested the while loop.

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -10,12 +10,13 @@ game_start = (input("Rock Paper Scissors! Would you like to play? "))
 while True:
     if game_start == "yes":
         yes_game = input("Rock, Paper, Scissors? ")
-        if yes_game == "Rock" "Paper" "Scissors":
-            print (random.choice(game_values))
+        
+        
+print(random.choice(game_values))
             
-        else:
-            print("Your input is invalid.")      
-    else:
-        print
-        break
+    #     else:
+    #         print("Your input is invalid.")      
+    # else:
+    #     print
+    #     break
     
