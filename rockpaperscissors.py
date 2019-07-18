8ee738f6819b545bb4844d1acace6f54d9737b3b
teevic80 <ogotvictoria@gmail.com>
2019-07-18 22:55:46 +0300

    Added conditionals to the program

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -11,13 +11,21 @@ while True:
     if game_start == "yes":
         yes_game = input("Rock, Paper, Scissors? ")
         print("My Choice is...")
-        print(random.choice(game_values))
+        mine = print(random.choice(game_values))
     else:
         if game_start == "no":
             print("Game Closed")
             break
         else:
             print("Your input is invalid")
+
+if mine == rock and yes_game == paper:
+    print ("You've won this round.")
+else:
+    print ("You loose this round.")
+
+if mine == scissors and yes_game == rock:
+    print ("You've won this round.")
 
 # if yes_game == "Rock" and     
         
