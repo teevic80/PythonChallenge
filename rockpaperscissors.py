f48f0ec8157fcbd0d1e8027766da989cbc17f465
teevic80 <ogotvictoria@gmail.com>
2019-07-18 23:20:47 +0300

    Trying out functions.

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -2,37 +2,48 @@
 
 import random
 
-game_values = ("Rock", "Paper", "Scissors")
+# game_values = ("Rock", "Paper", "Scissors")
 
-game_start = (input("Rock Paper Scissors! Would you like to play? "))
+# game_start = (input("Rock Paper Scissors! Would you like to play? "))
 
 
-while True:
-    if game_start == "yes":
-        yes_game = input("Rock, Paper, Scissors? ")
-        print("My Choice is...")
-        mine = print(random.choice(game_values))
+# while True:
+#     if game_start == "yes":
+#         yes_game = input("Rock, Paper, Scissors? ")
+#         print("My Choice is...")
+#         mine = print(random.choice(game_values))
+#     else:
+#         if game_start == "no":
+#             print("Game Closed")
+#             break
+#         else:
+#             print("Your input is invalid")
+
+#     if mine == yes_game:
+#         print("We've Tied")
+
+#     if mine == "rock" and yes_game == input("paper"):
+#         print ("You've won this round.")
+#     else:
+#         print ("You loose this round.")
+
+#     if mine == "scissors" and yes_game == "rock":
+#         print ("You've won this round.")
+#     else:
+#         print ("You loose this round")
+
+#     if mine == "paper" and yes_game == "scissors":
+#         print ("You've lost this round")
+#     else:
+#         print ("You've won this round")
+
+def to_play ():
+
+    input("Would you like to play? ")
+    while True:
+        print(input("Rock, Paper, Scissors? "))
     else:
-        if game_start == "no":
-            print("Game Closed")
-            break
-        else:
-            print("Your input is invalid")
+        print("Closing Game.....  Game CLOSED")
 
-    if mine == yes_game:
-        print("We've Tied")
 
-    if mine == "rock" and yes_game == input("paper"):
-        print ("You've won this round.")
-    else:
-        print ("You loose this round.")
-
-    if mine == "scissors" and yes_game == "rock":
-        print ("You've won this round.")
-    else:
-        print ("You loose this round")
-
-    if mine == "paper" and yes_game == "scissors":
-        print ("You've lost this round")
-    else:
-        print ("You've won this round")
+to_play()
