d271dcb53b3a24c53d0c65c99d4ba822e1d60d14
teevic80 <ogotvictoria@gmail.com>
2019-07-20 23:56:56 +0300

    This program prints the fibonacci sequence upto a given value.

--- fibonacci.py
+++ fibonacci.py
@@ -1,8 +1,10 @@
-#This program prints the fibonacci sequence.
+#This program prints the fibonacci sequence upto a given value.
+
+num = input("Type a number limit for the fibonacci sequence: ")
 
 x, y = 0, 1
 
-while y <= 1000:
+while y <= int(num):
     x, y = y, x + y
     print(x)
 
