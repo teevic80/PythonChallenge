371645cc0d97f1a5b6b5bfa33da6737208646de2
teevic80 <ogotvictoria@gmail.com>
2019-07-22 22:35:15 +0300

    Added a prompt.

--- guessingame.py
+++ guessingame.py
@@ -4,10 +4,11 @@ guessed = ("")
 
 my_guess = ("Orangutan")
 
-prompt = (input("Type in your word: "))
+prompt = input("Type in your word: ")
 
-while my_guess != guessed:
+while value in guessed != my_guess:
+    print(value + ": Your guess is incorrect.")
     print(prompt)
-    else:
-        if word in guessed == my_guess:
-            print("You guessed correct you win.")
+else:
+    if word in guessed == my_guess:
+        print("You guessed correct you win.")
