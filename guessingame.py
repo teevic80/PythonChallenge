6267b199f5ae6bf00c72779731dcf4e76f75daa0
teevic80 <ogotvictoria@gmail.com>
2019-07-22 22:43:07 +0300

    Added a while loop.

--- guessingame.py
+++ guessingame.py
@@ -1,14 +1,12 @@
 #This program lets you guess a name and check if it's the same as the one in the game.
 
-guessed = ("")
-
 my_guess = ("Orangutan")
 
 prompt = input("Type in your word: ")
 
-while value in guessed != my_guess:
-    print(value + ": Your guess is incorrect.")
-    print(prompt)
+while prompt != my_guess:
+    print(prompt + " is incorrect.")
+    print(input("Type in another word: "))
 else:
     if word in guessed == my_guess:
-        print("You guessed correct you win.")
+        print("You guessed correctly... You win!" + my_guess + " is the word.")
