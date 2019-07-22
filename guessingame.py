119518fbdaff7780b2c4f0cadaadca5f07a2cb54
teevic80 <ogotvictoria@gmail.com>
2019-07-22 22:27:40 +0300

    This program let's a user guess a name.

---
+++ guessingame.py
@@ -0,0 +1,13 @@
+#This program lets you guess a name and check if it's the same as the one in the game.
+
+guessed = ("")
+
+my_guess = ("Orangutan")
+
+prompt = (input("Type in your word: "))
+
+while my_guess != guessed:
+    print(prompt)
+    else:
+        if word in guessed == my_guess:
+            print("You guessed correct you win.")
