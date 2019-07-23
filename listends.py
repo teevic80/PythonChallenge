12d6cc9a83eb620c7b1605e28bbbd2b41157ee28
teevic80 <ogotvictoria@gmail.com>
2019-07-24 01:10:52 +0300

    This program is a function that returns the first and last values of users own list of numbers.

--- listends.py
+++ listends.py
@@ -3,13 +3,10 @@
 prompt = input(("Type in your numbers, separated by a space: "))
 
 x = prompt.split()
-print(x)
-print (x[0])
 y = x[::-1]
-print(y[0])
 
 def list_ends(first, last):
-    return x[0] 
-    return x[::-1]
+    print("Your first number is: " + x[0])
+    print("Your last number is: " + y[0])
 
-print(list_ends)
+list_ends(x, y)
