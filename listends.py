300d63ed5b2625e3af83148d6def2b7b209032fa
teevic80 <ogotvictoria@gmail.com>
2019-07-24 00:54:36 +0300

    Fixed errors.

--- listends.py
+++ listends.py
@@ -3,9 +3,13 @@
 prompt = input(("Type in your numbers, separated by a space: "))
 
 x = prompt.split()
+print(x)
+print (x[0])
+y = x[::-1]
+print(y[0])
 
 def list_ends(first, last):
     return x[0] 
     return x[::-1]
 
-list_ends(5, 6)
+print(list_ends)
