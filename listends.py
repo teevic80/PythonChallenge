5e87ba84cc3f45d3077c84c73152e234fc0d2a93
teevic80 <ogotvictoria@gmail.com>
2019-07-24 00:37:23 +0300

    Created a function for the program.

--- listends.py
+++ listends.py
@@ -1,2 +1,11 @@
 #This program is a function that returns the first and last values in a list.
 
+prompt = input(("Type in your numbers, separated by a space: "))
+
+x = prompt.split()
+
+def list_ends(first, last):
+    return x[0] 
+    return x[::-1]
+
+list_ends(5, 6)
