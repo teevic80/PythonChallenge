3caac6160fef7e2686c6f45ddceb10e26978286d
teevic80 <ogotvictoria@gmail.com>
2019-07-28 23:14:19 +0300

    This program adds the smallest numbers.

---
+++ addsmallest.py
@@ -0,0 +1,9 @@
+def sum_two_smallest_numbers(numbers):
+    numbers = input("Type in your numbers separated by a space: ")
+    # x = numbers.split()
+    list = [numbers]
+    print(list)
+    list.sort()
+    print(list)
+    print(x[0] + x[1])
+sum_two_smallest_numbers(4)
